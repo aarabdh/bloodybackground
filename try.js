6cc198921618f73327d831483b764a18e9dd7e1c
Aarabdh Tiwari <85110907+aarabdh@users.noreply.github.com>
2022-03-26 00:07:09 +0530

    Got rid of extra stuff

--- try.js
+++ try.js
@@ -1,5 +1,3 @@
-let x = 200;
-let y = 200;
 let extraCanvas;
 
 function setup() {
@@ -13,24 +11,13 @@ function setup() {
 
 function draw() {
     rectMode(CENTER);
-  // No trails!
     background(0);
-    x += random(-5, 5);
-    y += random(-5, 5);
-	
-  // trails
     for (let i=0; i<200; i++) {
-        extraCanvas.fill(255, 55, 100, 4);
+        extraCanvas.fill(255, 200, 250, 4);
         extraCanvas.noStroke();
         extraCanvas.ellipse(random(windowWidth), random(windowHeight), 60, 60);
         extraCanvas.fill(0, 4);
         extraCanvas.ellipse(random(windowWidth), random(windowHeight), 100, 100);
     }
-  
     image(extraCanvas, 0, 0);
-    fill(255, 0, 0);
-    stroke(255);
-    // if (mouseIsPressed()) noLoop();
-    // else loop();
-    // rect(x, y, 20, 20);
 }
